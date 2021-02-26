28b2e60aa846f25846c21690b26354aa3f01558a
Priyasoni11 <78486597+Priyasoni11@users.noreply.github.com>
2021-02-26 15:14:58 +0530

    Update EmailServiceTest.java

--- test/EmailServiceTest.java
+++ test/EmailServiceTest.java
@@ -6,18 +6,18 @@ class EmailServiceTest {
     private final EmailService emailService=EmailService.getInstance();
     private final Order order=new Order(8,"car",650000.0);
 
-    @org.junit.jupiter.api.Test
+    @Test
     void getInstance() {
         assertEquals(EmailService.getInstance(),emailService);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void sendEmail() {
         Executable executable=()->emailService.sendEmail(order);
         assertThrows(RuntimeException.class,executable);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testSendEmail() {
         assertTrue(emailService.sendEmail(order,"String"));
     }
